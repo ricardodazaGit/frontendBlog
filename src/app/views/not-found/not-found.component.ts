36b521724ff1c8cc46da67cf53d3816741d6db8c
Ricardo Daza <ricardo.daza@outlook.com>
2018-05-12 18:58:12 +0200

    feat: some improvements

--- src/app/views/not-found/not-found.component.ts
+++ src/app/views/not-found/not-found.component.ts
@@ -5,7 +5,7 @@ import { Router } from '@angular/router';
   selector: 'rp-not-found',
   template: `
     <p>Para cualquier duda o consulta, podéis escribirnos a blogjoselito@outlook.com</p>
-    <a routerLink="/">Go Home</a>
+    <a routerLink="/">Volver a inicio</a>
   `,
   styles: []
 })
