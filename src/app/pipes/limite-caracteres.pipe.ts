e728f744c6eee06b94c1482f9076cc658768ed22
Ricardo Daza <ricardo.daza@outlook.com>
2018-09-29 10:54:48 +0200

    Update npm dependencies

--- src/app/pipes/limite-caracteres.pipe.ts
+++ src/app/pipes/limite-caracteres.pipe.ts
@@ -6,7 +6,7 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class LimiteCaracteresPipe implements PipeTransform {
 
   transform(value: string): string {
-    return value.substring(0,500) + " ...";
+    return value.substring(0,250) + " ...";
   }
 
 }
--- src/app/views/publications/public-list.component.ts
+++ src/app/views/publications/public-list.component.ts
@@ -45,6 +45,7 @@ export class PublicListComponent implements OnInit {
 
   showDetail(_id: number) {
     this._router.navigate(['/publications',_id]);
+    //$("#.card-text").innerHtml(this.publications[_id].descriptionPost)
   }
 
 }
