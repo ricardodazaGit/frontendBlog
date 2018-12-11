d4e1ce169c2a2b571997e87110a1bcd22309c32c
Ricardo Daza <ricardo.daza@outlook.com>
2018-12-11 22:17:12 +0100

    Changes in routes to avoid 404 or bad results

--- src/app/views/publications/public-detail.component.ts
+++ src/app/views/publications/public-detail.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { Router, ActivatedRoute, Params } from '@angular/router';
 
 import { Publication } from './publication';
 import { PublicationsService } from './publications.service';
@@ -13,27 +13,48 @@ export class PublicDetail implements OnInit {
 
   publication: Publication;
   _id: number;
+  publicationNotExits: boolean = true;
 
   constructor(
+    private _router: Router,
     private _route: ActivatedRoute,
     private _publicService: PublicationsService
   ) { }
 
+  private obtainParamId(params: Params){
+    this._id = params['id'];
+  }
+
+  private obtainPublicationList(){
+    this._publicService.getPublicationsList$()
+      .subscribe(publications => {
+        for(let i in publications){
+          if(publications[i].id.toString() === this._id){
+            this.loadPublicationDetail(publications[i]);
+          }
+        }
+        this.redirectIfPublicationNotExits();
+      });
+  }
+
+  private loadPublicationDetail(publication: any){
+    this.publication = publication;
+    this.publicationNotExits = false;
+    document.title = publication.titlePost + ". Perdíz con reclamo";
+  }
+
+  private redirectIfPublicationNotExits(){
+    if(this.publicationNotExits) {
+      this._router.navigate(["/publications"]);
+    }
+  }
+
   ngOnInit() {
     this._route.params
-      .subscribe(params => {
-        this._id = params['id'];
-        this._publicService.getPublicationsList$()
-          .subscribe(publications => {
-            //this.publication = publication;
-            for(let i in publications){
-              if(publications[i].id.toString() === this._id){
-                this.publication = publications[i];
-                document.title = publications[i].titlePost + ". Perdíz con reclamo";
-              }
-            }
-          });
-      });
+    .subscribe(params => {
+      this.obtainParamId(params);
+      this.obtainPublicationList();
+    });
   }
 
 }
--- src/app/views/waits/waits-detail.component.ts
+++ src/app/views/waits/waits-detail.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router, Params } from '@angular/router';
 
 import { WaitPublication } from './wait-publication';
 import { WaitsService } from './waits.service';
@@ -13,26 +13,47 @@ export class WaitDetail implements OnInit {
 
   wait: WaitPublication;
   _id: number;
+  waitNotExits: boolean = true;
 
   constructor(
+    private _router: Router,
     private _route: ActivatedRoute,
     private _waitsService: WaitsService
   ) { }
 
+  private obtainParamId(params: Params){
+    this._id = params['id'];
+  }
+
+  private obtainWaitingList(){
+    this._waitsService.getWaitPublicationsList$()
+      .subscribe(waits => {
+        for(let i in waits){
+          if(waits[i].id.toString() === this._id){
+            this.loadWaitingDetail(waits[i]);
+          }
+        }
+        this.redirectIfWaitNotExits();
+      });
+  }
+
+  private loadWaitingDetail(wait: any){
+    this.wait = wait;
+    this.waitNotExits = false;
+    document.title = wait.titlePost + ". Esperas del jabalí";
+  }
+
+  private redirectIfWaitNotExits(){
+    if(this.waitNotExits) {
+      this._router.navigate(["/esperasDelJabali"]);
+    }
+  }
+
   ngOnInit() {
     this._route.params
       .subscribe(params => {
-        this._id = params['id'];
-        this._waitsService.getWaitPublicationsList$()
-          .subscribe(waits => {
-            //this.publication = publication;
-            for(let i in waits){
-              if(waits[i].id.toString() === this._id){
-                this.wait = waits[i];
-                document.title = waits[i].titlePost + ". Esperas del jabalí";
-              }
-            }
-          });
+        this.obtainParamId(params);
+        this.obtainWaitingList();
       });
   }
 
